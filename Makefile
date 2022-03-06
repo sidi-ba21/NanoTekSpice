bb63d65e6985a0b8f9a2eff15c3b411051fef895
Géraud <geraud@linux.home>
2022-03-06 18:14:32 +0100

    [UPDATE] Architecture of class

--- Makefile
+++ Makefile
@@ -9,31 +9,42 @@ NAME	=	nanotekspice
 
 SRC_DIR	=	src/
 
-IN_OUT_DIR = InputOutput/
+INPUT	=	Input/
 
-COMPONENT =	components/
+OUTPUT	=	Output/
+
+PIN		=	Pin/
+
+COMPONENT =	Components/
+
+ERROR	=	Error/
+
+PARSING	=	Parsing/
+
+CIRCUIT	=	Circuit/
 
 SRC		=	$(SRC_DIR)main.cpp						\
-			$(SRC_DIR)Gates.cpp						\
-			$(SRC_DIR)Parsing.cpp					\
-			$(SRC_DIR)Circuit.cpp					\
-			$(SRC_DIR)$(IN_OUT_DIR)Input.cpp		\
-			$(SRC_DIR)$(IN_OUT_DIR)Output.cpp		\
-			$(SRC_DIR)$(IN_OUT_DIR)InPin.cpp		\
-			$(SRC_DIR)$(IN_OUT_DIR)OutPin.cpp		\
-			$(SRC_DIR)$(IN_OUT_DIR)Pin.cpp			\
-			$(SRC_DIR)$(IN_OUT_DIR)CircuitPin.cpp	\
-			$(SRC_DIR)$(IN_OUT_DIR)True.cpp			\
-			$(SRC_DIR)$(IN_OUT_DIR)False.cpp		\
-			$(SRC_DIR)$(IN_OUT_DIR)Clock.cpp		\
+			$(SRC_DIR)$(CIRCUIT)Gates.cpp			\
+			$(SRC_DIR)$(CIRCUIT)Factory.cpp			\
+			$(SRC_DIR)$(CIRCUIT)Circuit.cpp			\
+			$(SRC_DIR)$(CIRCUIT)Circuitcreate.cpp	\
+			$(SRC_DIR)$(PARSING)Parsing.cpp			\
+			$(SRC_DIR)$(OUTPUT)Output.cpp			\
+			$(SRC_DIR)$(OUTPUT)OutPin.cpp			\
+			$(SRC_DIR)$(PIN)Pin.cpp					\
+			$(SRC_DIR)$(PIN)CircuitPin.cpp			\
+			$(SRC_DIR)$(INPUT)Input.cpp				\
+			$(SRC_DIR)$(INPUT)InPin.cpp				\
+			$(SRC_DIR)$(INPUT)True.cpp				\
+			$(SRC_DIR)$(INPUT)False.cpp				\
+			$(SRC_DIR)$(INPUT)Clock.cpp				\
 			$(SRC_DIR)$(COMPONENT)4071.cpp			\
 			$(SRC_DIR)$(COMPONENT)4081.cpp			\
 			$(SRC_DIR)$(COMPONENT)4001.cpp			\
 			$(SRC_DIR)$(COMPONENT)4011.cpp			\
 			$(SRC_DIR)$(COMPONENT)4030.cpp			\
 			$(SRC_DIR)$(COMPONENT)4069.cpp			\
-			$(SRC_DIR)Error.cpp						\
-			$(SRC_DIR)Factory.cpp					\
+			$(SRC_DIR)$(ERROR)Error.cpp				\
 
 OBJ		=	$(SRC:.cpp=.o)
 
--- src/Circuit/Circuitcreate.cpp
+++ src/Circuit/Circuitcreate.cpp
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2022
-** B-OOP-400-PAR-4-1-tekspice-geraud.deltour
+** Circuitcreate
 ** File description:
-** Circuit
+** Circuitcreate
 */
 
 #include "Circuit.hpp"
@@ -10,130 +10,13 @@
 #include <csignal>
 #include <exception>
 
-static bool _loop;
-
-nts::Circuit::Circuit(char *av) : file(av)
-{
-    createComponent();
-    createLink();
-    _command["loop"] = [](Circuit &c) {c.loop();};
-    _command["simulate"] = [](Circuit &c) {c.simulate();};
-    _command["dump"] = [](Circuit &c) {c.dump();};
-    _command["display"] = [](Circuit &c) {c.display();};
-}
-
-void nts::Circuit::display()
-{
-	std::cout << "tick: " << _tick << std::endl;
-	std::cout << "input(s):" << std::endl;
-	for (auto const &input : _inputs) {
-		input.lock()->display();
-	}
-	std::cout << "output(s):" << std::endl;
-	for (auto const &output : _outputs) {
-		output.lock()->display();
-	}
-}
-
-void nts::Circuit::simulate(size_t tick)
-{
-    if (tick != 0) {
-        _tick++;
-        reset();
-    }
-	for (auto const &output : _outputs) {
-		output.lock()->compute();
-	}
-}
-
-void stops(int)
-{
-    _loop = true;
-}
-
-void nts::Circuit::loop()
-{
-    _loop = false;
-	std::signal(SIGINT, stops);
-	while (!_loop) {
-		simulate();
-        display();
-    }
-	std::signal(SIGINT, SIG_DFL);
-}
-
-void nts::Circuit::dump()
-{
-	for (auto const &comp : _components) {
-		comp.second.get()->dump();
-	}
-}
-
-void nts::Circuit::set_value(const std::string &name, const std::string &value)
-{
-    if (value.compare("U") != 0) {
-        try {
-            std::stoi(value);
-        } catch (std::exception const &e) {
-            throw ValueReceiveError("Input value must be either 0 or 1 or U.");
-        }
-    }
-    for (auto &comp : _inputs) {
-        if (comp.lock()->getName().compare(name) == 0) {
-            comp.lock()->setValue(value);
-            return;
-        }
-    }
-    throw ValueReceiveError("Input error: Components doesn't exist.");
-}
-
-void nts::Circuit::run()
-{
-    std::string cmd;
-    simulate(0);
-    std::cout << "> " << std::flush;
-    while (std::getline(std::cin, cmd) && cmd.compare("exit")) {
-        try {
-        if (cmd.compare("") != 0)
-           execute(cmd);
-        } catch (std::exception const &e) {
-            std::cout << e.what() << std::endl;
-        }
-        std::cout << "> " << std::flush;
-    }
-}
-
-void nts::Circuit::execute(const std::string &name)
-{
-    std::string tmp = name;
-    tmp.erase(std::remove_if(tmp.begin(), tmp.end(), isspace), tmp.end());
-    auto it = _command.find(tmp);
-    if (tmp.find('=') != std::string::npos) {
-        auto str = tmp.substr(0, tmp.find('='));
-        auto value = tmp.substr(tmp.find('=') + 1);
-        set_value(str, value);
-        return;
-    }
-    else if (it == _command.end())
-        throw CommandError("Unknown command", "circuit::execute");
-    it->second(*this);
-}
-
-void nts::Circuit::reset()
-{
-    for (auto const &comp : _components)
-        comp.second.get()->reset();
-}
-
 void nts::Circuit::createComponent()
 {
     auto tmp = file.getChipsets();
     for (auto const &chipsets : tmp) {
-        if (_components.find(chipsets.second) != _components.end()) {
+        if (_components.find(chipsets.second) != _components.end())
 		    throw ComponentError("Components name already exists.", "create_component");
-	    }
-        if (special_create(chipsets.second, chipsets.first));
-        else {
+        if (!special_create(chipsets.second, chipsets.first)) {
             auto component
                 (_factories.createComponent(chipsets.first, chipsets.second));
             _components[chipsets.second] = std::move(component);
